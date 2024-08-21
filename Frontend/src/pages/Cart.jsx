e0044ce954c334f8051a532a051997a1e66385d3
Ved Shrivastava <vedshrivastava1357@gmail.com>
2024-08-21 16:32:42 +0530

    Fixed-Some-Bugs

--- Frontend/src/pages/Cart.jsx
+++ Frontend/src/pages/Cart.jsx
@@ -26,7 +26,7 @@ const Cart = () => {
               if (cartItems[item._id] > 0) {
                 return (
                   <div key={item._id} className="cart-items-title cart-items-item"> {/* Added key here */}
-                    <img src={url + "/images/" + item.image} alt="" />
+                    <img src={item.image} alt="" />
                     <p>{item.name}</p>
                     <p>₹{item.price}</p>
                     <div className="quantity-controls">
