9b4c7ecc664711873024273f56a5307dce0717e7
Ved Shrivastava <vedshrivastava1357@gmail.com>
2024-08-29 13:40:37 +0530

    height-adjusted

--- Backend/server.js
+++ Backend/server.js
@@ -21,12 +21,6 @@ app.use(cors())
 
 connectDB(process.env.MONGO_URI)
 
-// cloudinary.v2.config({
-//     cloud_name: process.env.CLOUD_NAME,
-//     api_key: process.env.CLOUD_API_KEY,
-//     api_secret: process.env.CLOUD_API_SECRET
-//   });
-
 app.use('/api/food', food);
 app.use('/api/user', user);
 app.use('/api/admin',admin)
